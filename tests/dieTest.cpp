54089449eca8329af1a7d1bd62953687355e898f
Mitchell Openlander <mopenla@bgsu.edu>
2017-04-14 21:52:00 -0400

    RED: gitValue() is not random

--- tests/dieTest.cpp
+++ tests/dieTest.cpp
@@ -8,4 +8,8 @@ TEST_CASE( "Die class", "[die]" ) {
     SECTION("Get Value") {
         REQUIRE( (d.getValue() > 0 && d.getValue() < 7) );
     }
+
+    SECTION("getValue() is random") {
+        REQUIRE( d.getValue() != d.getValue() );
+    }
 }
