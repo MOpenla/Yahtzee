23384cb31ff9197733acb9996a739ff6266e7b50
Mitchell Openlander <mopenla@bgsu.edu>
2017-04-18 13:57:04 -0400

    GREEN: defined dice class

---
+++ bin/dice.h
@@ -0,0 +1,13 @@
+#ifndef _DICE_H_
+#define _DICE_H_
+
+class dice {
+
+private:
+    
+
+public:
+    
+};
+
+#endif
---
+++ tests/diceTest.cpp
@@ -0,0 +1,7 @@
+#include "catch.hpp"
+#include "dice.h"
+#include <stdexcept>
+
+TEST_CASE( "dice class", "[dice]" ) {
+    dice d;
+}
