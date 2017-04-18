2355de77f7fac888e863934052f0f40374580187
Mitchell Openlander <mopenla@bgsu.edu>
2017-04-18 14:02:34 -0400

    RED: no getValues() method

--- tests/diceTest.cpp
+++ tests/diceTest.cpp
@@ -1,7 +1,16 @@
 #include "catch.hpp"
 #include "dice.h"
 #include <stdexcept>
+#include <vector>
 
 TEST_CASE( "dice class", "[dice]" ) {
     dice d;
+
+    SECTION("the values of unrolled dice are 0") {
+        std::vector<int> vals = d.getValues();
+
+        for(int i = 0; i < vals.size(); i++) {
+            REQUIRE(vals[i] == 0);
+        }
+    }
 }
