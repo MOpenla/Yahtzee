ba84af165a2d06ac4bea400c569807e5f0e0d44b
Mitchell Openlander <mopenla@bgsu.edu>
2017-04-18 14:25:50 -0400

    GREEN: implemented roll()

--- bin/dice.h
+++ bin/dice.h
@@ -1,14 +1,35 @@
 #ifndef _DICE_H_
 #define _DICE_H_
 
+#include "die.h" //TODO: this doesn't need to be in here
+
 class dice {
 
 private:
-    
+    std::vector<die> dies;
 
 public:
-    std::vector<int> getValues() { std::vector<int> v (5,0); return v; }
-    void roll() {};
+    dice() { 
+        for(int i = 0; i < 6; i++) {
+            die d;
+            dies.push_back(d);
+        }
+    };
+
+    std::vector<int> getValues() { 
+        std::vector<int> temp;
+
+        for(int i = 0; i < dies.size(); i++) {
+            temp.push_back( dies[i].getValue() );
+        }
+
+        return temp; 
+    }
+    void roll() { 
+        for(int i = 0; i < dies.size(); i++) {
+            dies[i].roll();
+        }
+    };
 };
 
 #endif
