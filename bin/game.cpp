0f525fbdb9adc63ac0e2fda7b2d2efd2366ada9e
BenForster <bforste@bgsu.edu>
2017-04-18 21:12:58 -0400

    GREEN: Testing validChoice function of game class

--- bin/game.cpp
+++ bin/game.cpp
@@ -6,8 +6,54 @@
 
 using namespace std;
 
+//ScoreCard ScoreCard;
+
 game::game(){
 
+}
+
+void Game::Play(){
+    //Dice dice;
+    int currentRound = 1;
+    int scoreArray[13];
+    bool played[13];
+    string choice;
+    int diceRolls;
+    string rrInput;
+    int intChoice;
+    
+    while(keepPlaying(1)) //while(keepPlaying(ScoreCard.numberOfUnplayedCategories()))
+    {
+        cout << "-------------------- Round " << currentRound
+        << " --------------------" << endl;
+        
+        cout << endl << "Rolling the dice." << endl;
+        //dice.roll();
+        diceRolls = 1;
+        
+        cout << "Which category number would you like to play? ";
+        cin >> choice;
+        intChoice = validChoice(choice);
+        
+        while(played[intChoice-1])
+        {
+            cout <<"Sorry, that category has already been played. Please enter another category number: ";
+            cin >> choice;
+            intChoice = validChoice(choice);
+        }
+        
+        played[intChoice-1] = true;
+        
+        //setScore(catNum, dice);
+    }
+    
+    cout << endl;
+    cout << "**************************************************" << endl;
+    cout << "*            Round " << currentRound << " Scored ";
+    cout << scoreArray[catNum-1] << " points            *" << endl;
+    cout << "**************************************************" << endl << endl;
+    
+    currentRound++;
 }
 
 void Display(int numberOfUnplayedCategories[])
--- tests/gameTests.cpp
+++ tests/gameTests.cpp
@@ -1,6 +1,5 @@
 #include "catch.hpp"
 #include "game.cpp"
-#include "die.h"
 #include <string>
 #include <stdexcept>
 
@@ -20,6 +19,12 @@ TEST_CASE( "keepPlaying function of game class", "[game]" ) {
 TEST_CASE( "validChoice function of game class", "[game]" ) {
     game game;
     string choice = "1";
+    
+    SECTION("Game takes in a string and returns a valid choice integer") {
+        REQUIRE(validChoice(choice)>0);
+        REQUIRE(validChoice(choice)<14);
+    }
+    choice = "13";
     
     SECTION("Game takes in a string and returns a valid choice integer") {
         REQUIRE(validChoice(choice)>0);
