b37c6150e64424dc1a59302e97c40801fd8603de
BenForster <bforste@bgsu.edu>
2017-04-18 20:04:46 -0400

    RED: Testing validChoice function of game class

--- tests/gameTests.cpp
+++ tests/gameTests.cpp
@@ -1,9 +1,10 @@
 #include "catch.hpp"
 #include "game.cpp"
 #include "die.h"
+#include <string>
 #include <stdexcept>
 
-TEST_CASE( "Game class", "[game]" ) {
+TEST_CASE( "keepPlaying function of game class", "[game]" ) {
     game game;
     int numberOfUnplayedCategories = 12;
 
@@ -12,7 +13,17 @@ TEST_CASE( "Game class", "[game]" ) {
     }
     numberOfUnplayedCategories = 0;
     SECTION("Game stops playing once number of un-played categories is 0") {
-	REQUIRE(!keepPlaying(numberOfUnplayedCategories));
+        REQUIRE(!keepPlaying(numberOfUnplayedCategories));
+    }
+}
+
+TEST_CASE( "validChoice function of game class", "[game]" ) {
+    game game;
+    string choice = "1";
+    
+    SECTION("Game takes in a string and returns a valid choice integer") {
+        REQUIRE(validChoice(choice)>0);
+        REQUIRE(validChoice(choice)<14);
     }
 }
 
